85c04f277b542f06f17a89df88db2985576f4934
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-09-07 02:26:32 +0530

    heroku 1

--- App.js
+++ App.js
@@ -15,6 +15,10 @@ const authRoute = require('./routes/auth')
 app.use('/account', accountRoute);
 app.use('/user', authRoute);
 
+app.get('cron-job',(req,res) => {
+    res.send("This is an api for Cron-Job")
+})
+
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mandidb-sdbcm.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
